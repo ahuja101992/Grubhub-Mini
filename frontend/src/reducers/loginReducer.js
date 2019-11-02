b60e962a11b44c8aee9510adbca181bd3d4e85d2
ahuja101992 <ahuja101992@gmail.com>
2019-11-02 10:59:40 -0700

    chat changes in action and reducer

--- frontend/src/reducers/loginReducer.js
+++ frontend/src/reducers/loginReducer.js
@@ -34,10 +34,10 @@ const initialState = {
 export default function loginReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_LOGIN:
-      // console.log("action ++++" + JSON.stringify(action.payload.data));
+      console.log("action ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        authFlag: action.payload.data.authFlag
+        errMsg: action.payload.data.result.response.errMsg,
+        authFlag: action.payload.data.result.response.authFlag
       });
     case GOTO_SIGNUP:
       console.log("GOTO_SIGNUP");
@@ -48,8 +48,8 @@ export default function loginReducer(state = initialState, action) {
     case SIGN_UP:
       console.log("action data" + JSON.stringify(action));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        success: action.payload.data.success
+        errMsg: action.payload.data.result.response.errMsg,
+        success: action.payload.data.result.response.success
       });
     case GOTO_LOGIN:
       return Object.assign({}, state, {
@@ -59,15 +59,15 @@ export default function loginReducer(state = initialState, action) {
     case SIGN_IN_R:
       console.log("step2" + JSON.stringify(action));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        success: action.payload.data.success,
-        authFlag: action.payload.data.authFlag
+        errMsg: action.payload.data.result.response.errMsg,
+        success: action.payload.data.result.response.success,
+        authFlag: action.payload.data.result.response.authFlag
       });
     case SIGN_UP_R:
       console.log("step2" + action.payload);
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        success: action.payload.data.success
+        errMsg: action.payload.data.result.response.errMsg,
+        success: action.payload.data.result.response.success
       });
     case GOTO_PROFILE:
       return Object.assign({}, state, {
@@ -80,11 +80,12 @@ export default function loginReducer(state = initialState, action) {
       return Object.assign({}, state, {
         errMsg: action.payload.data.errMsg,
 
-        success: action.payload.data.success,
-        first_name: action.payload.data.first_name,
-        last_name: action.payload.data.last_name,
-        email_id: action.payload.data.email_id,
-        phone_num: action.payload.data.phone_num
+        success: action.payload.data.result.response.success,
+        first_name: action.payload.data.result.response.first_name,
+        last_name: action.payload.data.result.response.last_name,
+        email_id: action.payload.data.result.response.email_id,
+        phone_num: action.payload.data.result.response.phone_num,
+        profile_image: action.payload.data.result.response.profile_image
       });
     case GOTO_PROFILEEDIT:
       return Object.assign({}, state, {
@@ -95,41 +96,47 @@ export default function loginReducer(state = initialState, action) {
     case UPD_PROFILE:
       console.log("update profile");
       return Object.assign({}, state, {
-        success: action.payload.data.success,
-        errMsg: action.payload.data.errMsg,
-        first_name: action.payload.data.first_name,
-        last_name: action.payload.data.last_name,
+        success: action.payload.data.result.response.success,
+        errMsg: action.payload.data.result.response.errMsg,
+        first_name: action.payload.data.result.response.first_name,
+        last_name: action.payload.data.result.response.last_name,
+        profile_image: action.payload.data.result.response.profile_image,
         toProfileEdit: action.toProfileEdit
       });
     case GET_RESPROFILE:
       console.log("Get RES Profile" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        success: action.payload.data.success,
-        first_name: action.payload.data.first_name,
-        last_name: action.payload.data.last_name,
-        email_id: action.payload.data.email_id,
-        phone_num: action.payload.data.phone_num,
-        rest_name: action.payload.data.resturant_name,
-        rest_zip: action.payload.data.resturant_zipcode,
-        cuisine: action.payload.data.cuisine
+        errMsg: action.payload.data.result.response.errMsg,
+        success: action.payload.data.result.response.success,
+        first_name: action.payload.data.result.response.first_name,
+        last_name: action.payload.data.result.response.last_name,
+        email_id: action.payload.data.result.response.email_id,
+        phone_num: action.payload.data.result.response.phone_num,
+        rest_name: action.payload.data.result.response.resturant_name,
+        rest_zip: action.payload.data.result.response.resturant_zipcode,
+        cuisine: action.payload.data.result.response.cuisine,
+        profile_image: action.payload.data.result.response.profile_image
       });
     case UPD_RESPROFILE:
       console.log("update res profile" + JSON.stringify(action));
       return Object.assign({}, state, {
-        success: action.payload.data.success,
-        errMsg: action.payload.data.errMsg,
-        first_name: action.payload.data.first_name,
-        last_name: action.payload.data.last_name,
-        toProfileEdit: action.toProfileEdit
+        success: action.payload.data.result.response.success,
+        errMsg: action.payload.data.result.response.errMsg,
+        first_name: action.payload.data.result.response.first_name,
+        last_name: action.payload.data.result.response.last_name,
+        phone_num: action.payload.data.result.response.phone_num,
+        rest_name: action.payload.data.result.response.resturant_name,
+        rest_zip: action.payload.data.result.response.resturant_zipcode,
+        // toProfileEdit: action.result.response.toProfileEdit,
+        profile_image: action.payload.data.result.response.profile_image
       });
     case SEARCH_REST:
       // console.log("Inside Search" + JSON.stringify(action));
       return Object.assign({}, state, {
-        success: action.payload.data.success,
-        errMsg: action.payload.data.errMsg,
-        getRest: action.payload.data.getRest,
-        restaurants: action.payload.data.restaurants
+        success: action.payload.data.result.response.success,
+        errMsg: action.payload.data.result.response.errMsg,
+        getRest: action.payload.data.result.response.getRest,
+        restaurants: action.payload.data.result.response.restaurants
       });
     default:
       return state;
--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -12,7 +12,11 @@ import {
   GET_DISHESBUY,
   PLACE_ORDER,
   BCURR_ORDER,
-  BPAST_ORDER
+  BPAST_ORDER,
+  ADD_SECTION,
+  GET_SECTION,
+  DEL_SECTION,
+  GET_CHATLIST
 } from "../actions/types";
 
 const initialState = {
@@ -36,7 +40,8 @@ const initialState = {
   orderSuccess: null,
   orderResult: null,
   getBCurrSuccess: null,
-  bcurrOrds: null
+  bcurrOrds: null,
+  insertSecSuccess: null
 };
 
 export default function orderReducer(state = initialState, action) {
@@ -45,124 +50,161 @@ export default function orderReducer(state = initialState, action) {
     case ADD_DISH:
       // console.log("action ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        dishSuccess: action.payload.data.dishSuccess,
-        success: action.payload.data.success
+        errMsg: action.payload.data.result.response.errMsg,
+        dishSuccess: action.payload.data.result.response.dishSuccess,
+        success: action.payload.data.result.response.success
       });
     case GET_DISHES:
-      // console.log("action ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getDishSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        dishes: action.payload.data.dishes
+        errMsg: action.payload.data.result.response.errMsg,
+        getDishSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        dishes: action.payload.data.result.response.sections
       });
     case GET_CURRDISHES:
       console.log("action ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        dishDetails: action.payload.data.dishDetails
+        errMsg: action.payload.data.result.response.errMsg,
+        getSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        dishDetails: action.payload.data.result.response.dishDetails
       });
     case DEL_DISHES:
       console.log("DEL_DISH ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        delSuccess: action.payload.data.delSuccess,
-        success: action.payload.data.success,
-        dishDetails: action.payload.data.dishDetails,
-        result: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        delSuccess: action.payload.data.result.response.delSuccess,
+        success: action.payload.data.result.response.success,
+        dishDetails: action.payload.data.result.response.dishDetails,
+        result: action.payload.data.result.response.result
       });
     case UPDATE_DISH:
       console.log("UPD_DISH ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        delSuccess: action.payload.data.delSuccess,
-        success: action.payload.data.success,
-        dishDetails: action.payload.data.dishDetails,
-        result: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        delSuccess: action.payload.data.result.response.delSuccess,
+        success: action.payload.data.result.response.success,
+        dishDetails: action.payload.data.result.response.dishDetails,
+        result: action.payload.data.result.response.result
       });
     case RCURR_ORDER:
       console.log("RCURR_ORDER ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getCurrSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        currOrds: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        getCurrSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        currOrds: action.payload.data.result.response.result
       });
     case RPAST_ORDER:
       console.log("RPAST_ORDER ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getPastSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        pastOrds: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        getPastSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        pastOrds: action.payload.data.result.response.result
       });
     case GET_ITEMS:
       console.log("GET_ITEMS ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getSuccessItems: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        items: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        getSuccessItems: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        items: action.payload.data.result.response.result
       });
     case CANCEL_ORDER:
       console.log("CANCEL_ORD ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        cancelSuccess: action.payload.data.updateSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        cancelRes: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        cancelSuccess: action.payload.data.result.response.updateSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        cancelRes: action.payload.data.result.response.result
       });
     case UPDATE_STATUS:
       console.log("Update++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        updateSuccess: action.payload.data.updateSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        updateRes: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        updateSuccess: action.payload.data.result.response.updateSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        updateRes: action.payload.data.result.response.result
       });
     case GET_DISHESBUY:
       console.log("GET_DISHESBUY++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getBuyDishSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        dishesBuy: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        getBuyDishSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        dishesBuy: action.payload.data.result.response.result
       });
     case PLACE_ORDER:
-      console.log("GET_DISHESBUY++++" + JSON.stringify(action.payload.data));
+      console.log("PLACE_ORDER++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        orderSuccess: action.payload.data.orderSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        orderResult: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        orderSuccess: action.payload.data.result.response.orderSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        orderResult: action.payload.data.result.response.result
       });
     case BCURR_ORDER:
-      console.log("BCURR_ORDER ++++" + JSON.stringify(action.payload.data));
+      console.log(
+        "It was here too BCURR_ORDER ++++" + JSON.stringify(action.payload.data)
+      );
       return Object.assign({}, state, {
-        errMsg: action.payload.data.errMsg,
-        getBCurrSuccess: action.payload.data.getSuccess,
-        success: action.payload.data.success,
-        err: action.payload.data.err,
-        bcurrOrds: action.payload.data.result
+        errMsg: action.payload.data.result.response.errMsg,
+        getBCurrSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        bcurrOrds: action.payload.data.result.response.result
       });
     case BPAST_ORDER:
       console.log("BPAST_ORDER ++++" + JSON.stringify(action.payload.data));
       return Object.assign({}, state, {
+        errMsg: action.payload.data.result.response.errMsg,
+        getBPastSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        bpastOrds: action.payload.data.result.response.result
+      });
+    case ADD_SECTION:
+      console.log("ADD_SECTION ++++" + JSON.stringify(action.payload.data));
+      return Object.assign({}, state, {
+        errMsg: action.payload.data.result.response.errMsg,
+        addSecSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        results: action.payload.data.result.response.result
+      });
+    case GET_SECTION:
+      // console.log("GET_SECTION ++++" + JSON.stringify(action.payload.data));
+      return Object.assign({}, state, {
+        errMsg: action.payload.data.result.response.errMsg,
+        getSecSuccess: action.payload.data.result.response.getSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        sections: action.payload.data.result.response.sections
+      });
+    case DEL_SECTION:
+      console.log("DEL_SECTION ++++" + JSON.stringify(action.payload.data));
+      return Object.assign({}, state, {
+        errMsg: action.payload.data.result.response.errMsg,
+        delSecSuccess: action.payload.data.result.response.delSecSuccess,
+        success: action.payload.data.result.response.success,
+        err: action.payload.data.result.response.err,
+        result: action.payload.data.result.response.result
+      });
+    case GET_CHATLIST:
+      // console.log("GET_CHATLIST" + JSON.stringify(action.payload.data));
+      return Object.assign({}, state, {
         errMsg: action.payload.data.errMsg,
-        getBPastSuccess: action.payload.data.getSuccess,
+        getchatSuccess: action.payload.data.getSuccess,
         success: action.payload.data.success,
         err: action.payload.data.err,
-        bpastOrds: action.payload.data.result
+        chat: action.payload.data.chat
       });
     default:
       return state;
